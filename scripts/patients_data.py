3dc0acf56406ec65cd5432adc301f0ad0ee77bbc
bels4 <belsalovephysics@gmail.com>
2020-03-16 03:14:22 +0300

    fix

--- scripts/patients_data.py
+++ scripts/patients_data.py
@@ -4,21 +4,34 @@ import os
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC, Gapped
 
+import pandas as pd
+import json
+import os
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC, Gapped
+
+def get_patients():
+    return [f'p{i}' for i in range(1, 12)]
+
+def get_regions():
+    return ["V3", "PR", "psi", "vpr", "vpu", "p1", "p2", "p6", "p7", "p15", "p17", "RRE"]
 
 class Patient:
     """ Класс для хранения данных пациента """
 
-    def __init__(self, pid, data_path='data/hivevo'):
+    def __init__(self, pid, data_path='data/hivevo', verbosity=False):
         """
         Args:
             pid (str): ID пациента
             data_path (str): путь до данных
+            verbosity (bool): говорливость кода
         """
         self.id = pid
         self.reference = Reference(data_path, patient=pid)
         self.status = None
         self.data_path = data_path
         self.regions = None
+        self.verbosity = verbosity
 
         # сразу записываем регионы
         self.init_regions()
@@ -38,8 +51,12 @@ class Patient:
 
             # если не смогли скачать данные, то пишем об этом
             if type(region_file) == dict:
-                print(f'No haplotype for patient {self.id} for region {name}')
+                
+                if self.verbosity:
+                    print(f'No haplotype for patient {self.id} for region {name}')
+                
                 continue
+            
 
             # временный датасет
             region = pd.DataFrame(data=region_file)
@@ -83,7 +100,7 @@ class Reference:
                 json_file = json.load(f)
 
                 # удаляем ненужные колонки
-                t = pd.DataFrame(data=json_file).drop(['name', 'description'], axis=1)
+                t = pd.DataFrame(data=json_file).drop(['name', 'description', 'framestart', 'len'], axis=1)
 
                 # делим объединённые колонки на отдельные
                 t = pd.concat([t.drop(['features'], axis=1), t.features.apply(pd.Series)], axis=1)
@@ -92,18 +109,34 @@ class Reference:
                 t.location = t.location.apply(pd.Series)
 
                 # вырезаем последовательность
-                t['region_seq'] = t.apply(lambda x: x.seq[x.location[0]:x.location[1]], axis=1)
+                t['region_seq'] = t.apply(lambda x: x.seq[x.location[0]:x.location[1]].rstrip().lstrip(), axis=1)
+                
 
                 # переименовываем для единообразия
                 t.rename(mapper={'region_seq': 'sequence', 'seq': 'full_reference'}, axis=1, inplace=True)
                 t['translated'] = t.sequence.apply(lambda x: Seq(x, Gapped(IUPAC.unambiguous_dna)).ungap().translate())
+                
+                # удаляем лишнее
+                t = t.drop(['location', 'full_reference', 'type'], axis=1)
+                
+                # добавляем колонки, чтобы соединить с датасетом гаплотипов
+                num_row = t.shape[0]
+                
+                frequency = [100.0 for i in range(num_row)]
+                days = [0 for i in range(num_row)]
+                nreads = [None for i in range(num_row)]
+                
+                t['frequency'] = frequency
+                t['days'] = days
+                t['nreads'] = nreads                
+
                 self.reference_df = pd.concat([self.reference_df, t], ignore_index=True)
 
         # если это делалось для одного пациента, то сразу отдаём объект с его регионами
         if patient:
             self.region = Region(self.reference_df)
 
-    def patient(self, patient, region=None):
+    def get_patient(self, patient, region=None):
         """
         Функция для извлечения данных о пациенте или пациенте и регионе, если используем класс отдельно
         Args:
@@ -114,10 +147,10 @@ class Reference:
             pd.DataFrame: отфильтрованные данные
         """
         if region:
-            return self.reference_df.loc[(self.reference_df.id == f'reference_{patient}') &
+            return self.reference_df.drop(['id'], axis=1).loc[(self.reference_df.id == f'reference_{patient}') &
                                          (self.reference_df.name == region)]
         else:
-            return self.reference_df.loc[self.reference_df.id == f'reference_{patient}']
+            return self.reference_df.drop(['id'], axis=1).loc[self.reference_df.id == f'reference_{patient}']
 
 
 class Region:
